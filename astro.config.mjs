5dc49c5a0d014ea70dbd84e3910abd23daf8d97c
urzuaf <urzua.fernando.f@gmail.com>
2025-01-10 11:49:50 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://pathalgebra.github.io/',
+  base: 'pathdbweb',
+  integrations: [tailwind(), svelte()]
+});
